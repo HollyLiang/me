d8b21397f6262212467e0ffa467bd1357d649b6e
Holly Liang <holly86206@gmail.com>
2020-06-30 16:50:35 +0800

    Add @delon/mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// #region Mock
+import { AlainConfig, ALAIN_CONFIG } from '@delon/util';
+import { DelonMockModule } from '@delon/mock';
+import * as MOCKDATA from '../../_mock';
+
+const alainConfig: AlainConfig = {
+  mock: { data: MOCKDATA }
+};
+// #endregion
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DelonMockModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: ALAIN_CONFIG, useValue: alainConfig },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
