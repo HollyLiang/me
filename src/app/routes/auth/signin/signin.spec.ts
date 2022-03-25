b0909d5b92198b2c4be6633491796ff7297c0c1d
Holly Liang <holly86206@gmail.com>
2022-03-25 11:05:51 +0800

    Update to Angular 11

--- src/app/routes/auth/signin/signin.spec.ts
+++ src/app/routes/auth/signin/signin.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SignInPage } from './signin';
 
@@ -6,7 +6,7 @@ describe('SignInPage', () => {
   let component: SignInPage;
   let fixture: ComponentFixture<SignInPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SignInPage ]
     })
